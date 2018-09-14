0c709d103f1ea60e44f3ebfd83284d9166ae7c74
Corry TERBLANCHE <cterblan@e4r13p6.wethinkcode.co.za>
2018-09-14 17:00:46 +0200

    added libraries

---
+++ inc/ft_minishell.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_minishell.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/14 15:46:19 by cterblan          #+#    #+#             */
+/*   Updated: 2018/09/14 15:48:15 by cterblan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MINISHELL_H
+# define FT_MINISHELL_H
+
+#include "../lib/ft_printf/inc/ft_printf.h"
+
+#endif
