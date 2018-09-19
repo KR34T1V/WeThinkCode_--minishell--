b5f97a92b317779d3e8332612338afaace4d4f8a
Corry TERBLANCHE <cterblan@e4r13p6.wethinkcode.co.za>
2018-09-19 17:12:16 +0200

    fixed seg

--- src/min_sys_scan_dir.c
+++ src/min_sys_scan_dir.c
@@ -6,7 +6,7 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 12:09:58 by cterblan          #+#    #+#             */
-/*   Updated: 2018/09/19 15:41:55 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/09/19 17:10:13 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,20 @@ char	*min_sys_scan_dir(char **av, char ***env)
 
 	i = -1;
 	bins = min_sys_set_path(env);
-	while (bins[i] && ft_isdir(bins[++i]))
-	{
-		dir = opendir(bins[i]);
-		while ((ent = readdir(dir)) != NULL)
+	if (bins)
+		while (bins[i] && ft_isdir(bins[++i]))
 		{
-			if (av[0] && 0 == ft_strcmp(av[0], ent->d_name))
-			{
-				path = ft_strjoin(bins[i], av[0]);
-				ft_free2d_char(bins);
-				closedir(dir);
-				return (path);
-			}
+			dir = opendir(bins[i]);
+			while ((ent = readdir(dir)) != NULL)
+				if (av[0] && 0 == ft_strcmp(av[0], ent->d_name))
+				{
+					path = ft_strjoin(bins[i], av[0]);
+					ft_free2d_char(bins);
+					closedir(dir);
+					return (path);
+				}
+			closedir(dir);
 		}
-		closedir(dir);
-	}
 	ft_free2d_char(bins);
 	return (NULL);
 }
