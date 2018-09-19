f9e86b322e39be0c2f73f216e96df6cb0f665806
Corry TERBLANCHE <cterblan@e4r13p6.wethinkcode.co.za>
2018-09-19 11:45:17 +0200

    fixed cd solo

--- src/min_cmd_cd.c
+++ src/min_cmd_cd.c
@@ -6,23 +6,20 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 09:23:00 by cterblan          #+#    #+#             */
-/*   Updated: 2018/09/19 09:45:03 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/09/19 11:43:26 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int		min_cmd_cd(int ac, char **av, char ***env)
+static void	min_cmd_cd_av(char *av, char ***env)
 {
 	int		i;
 	char	*tmp;
 
-	i = 0;
-	min_shell_path(&av, env);
-	if (ac <= 1)
-		ft_printf("\e[91mUsage:\n\tcd [PATH]\e[96m\n");
-	else if (av[1] && 0 == chdir(av[1]))
+	if (av && 0 == chdir(av))
 	{
+		i = 0;
 		tmp = min_env_get_val(env, "PWD=");
 		min_env_set_path(env, "OLDPWD=", tmp);
 		free(tmp);
@@ -33,5 +30,27 @@ int		min_cmd_cd(int ac, char **av, char ***env)
 	}
 	else
 		ft_printf("\e[91mERROR: Invalid Path/Permissions\e[96m\n");
+}
+
+int		min_cmd_cd(int ac, char **av, char ***env)
+{
+	char	*tmp;
+
+	if (ac <= 1)
+	{
+		tmp = min_env_get_val(env, "HOME=");
+		if (tmp)
+		{
+			min_cmd_cd_av(tmp, env);
+			ft_strdel(&tmp);
+		}
+		else
+			ft_printf("\e[91mERROR: Home variable not found!\e[96m\n");
+	}
+	else if (ac > 1)
+	{
+		min_shell_path(&av, env);
+		min_cmd_cd_av(av[1], env);
+	}
 	return (0);
 }
